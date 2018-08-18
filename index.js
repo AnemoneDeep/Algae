86bdda58a4447437e580805a7650530350d7db67
letbewith <letbewith@gmail.com>
2018-08-18 19:36:09 +0700

    :sleeping:

--- index.js
+++ index.js
@@ -16,3 +16,20 @@
  ﾉﾍ　　　　　　ヽ
  * */
 let VERSION = '1.0.0';
+let algea = {VERSION};
+let {getType, _isType} = require('./lib/core/type')
+let {httpReq, httpGet} = require('./lib/net')
+
+algea.each = require('./lib/arithmetic/traverse/each')
+algea.asQueue = require('./lib/arithmetic/queue/async.queue')
+algea.len = require('./lib/core/{}[]/length')
+algea.md5 = require('./lib/arithmetic/md5')
+
+
+algea.getType = getType
+algea.isType = _isType
+
+algea.httpReq = httpReq
+algea.httpGet = httpGet
+
+module.exports = algea;
