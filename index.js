7ddcec147ad9867c58830aede9c6f61f820340b1
letbewith <letbewith@gmail.com>
2019-09-10 22:18:05 +0700

    feat: request add userAgent pc h5

--- index.js
+++ index.js
@@ -19,18 +19,23 @@ let VERSION = '1.0.0';
 let algea = {VERSION};
 let {getType, _isType} = require('./lib/core/type')
 let {httpReq, httpGet} = require('./lib/net')
+let time = require('./lib/time')
 
 algea.each = require('./lib/arithmetic/traverse/each')
 algea.asQueue = require('./lib/arithmetic/queue/async.queue')
 algea.len = require('./lib/core/{}[]/length')
 algea.md5 = require('./lib/arithmetic/md5')
 algea.restfull = require('./lib/restfull')
-
+algea.lowDBClient = require('./lib/aDB')
 
 algea.getType = getType
 algea.isType = _isType
 
 algea.httpReq = httpReq
 algea.httpGet = httpGet
+
+algea.axios = require('axios')
+
+algea.time = time
 
 module.exports = algea;
